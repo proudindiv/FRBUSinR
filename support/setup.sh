0cff5e61a5442f9b7ef17c44b19e4b219aeb9ad2
Gary Young <proudindiv@gmail.com>
2016-06-28 17:48:25 -0600

    reorged support

--- support/setup.sh
+++ support/setup.sh
@@ -2,3 +2,4 @@ mkdir -p srcEview/data.only.package
 mkdir -p srcEview/{frbus.package,mce.solve.package,state.space.package}
 mkdir -p srcEview/frbus.package/{addins,mods,programs,subs}
 mkdir -p srcEview/frbus.package/addins/{ld.frbus.cfs,ld.frbus.eqs,ld.mce.cfs,ld.mce.eqs,ld.some.eqs,ld.varinfo}
+mkdir -p support
